189c3adff2c0f2bdc0371b2be07022cd0e7887b4
Ishan19agarwal <ishanagarwal0220@gmail.com>
2023-11-21 21:18:40 +0530

    cors issue

--- services/emailServices.js
+++ services/emailServices.js
@@ -1,28 +1,3 @@
-// const nodemailer = require('nodemailer')
-// require('dotenv').config()
-
-// const sendMail = async ({from, to, subject, text, html}) =>{
-//     let transporter = nodemailer.createTransport(
-//         {
-//             host: process.env.SMTP_HOST,
-//             port: process.env.SMTP_PORT,
-//             secure: false,
-//             auth: {
-//                 user: process.env.SMTP_LOGIN,
-//                 pass: process.env.SMTP_PASS
-//             }
-//         }
-//     )
-
-//     let info = await transporter.sendMail({
-//         from: `QRush <${from}>`,
-//         to: to,
-//         subject: subject,
-//         text: text,
-//         html: html
-//     })
-// }
-
 const { Resend } = require("resend");
 
 const resend = new Resend("re_AjDFC5VT_B5cticF5gPep8ECpt37cnEKf");
